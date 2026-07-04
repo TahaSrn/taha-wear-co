c442d01ca22d9678001f0a1d7da7f2ced9c855de
TahaSrn <tahaahmadi0403@gmail.com>
2026-07-04 23:06:09 +0330

    Banner updated

--- src/ui/Banner.jsx
+++ src/ui/Banner.jsx
@@ -4,7 +4,7 @@ function Banner() {
       <div className="w-full max-w-7xl mx-auto px-2 md:px-4">
         <div className="w-full aspect-100/60 md:aspect-100/60 lg:aspect-100/38 overflow-hidden rounded-xl border border-caffee-300">
           <img
-            src="/banner.png"
+            src="/banner.jpg"
             alt="banner"
             className="w-full h-full object-cover object-[10%_center] md:object-center"
           />
